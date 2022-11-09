72910b51a36c89bb3c33d9fe88a2014ff134b837
letusgitcommit <davidvaughn006@gmail.com>
2022-11-08 20:05:24 -0600

    made the Todo model and TodoCreateModelForm

---
+++ todos/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
+from .models import Todo
+
+
+class TodoCreateModelForm(forms.ModelForm):
+    class Meta:
+        model = Todo
+        fields = ['text']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-todo_create_form'
+        self.helper.form_method = 'post'
+        custom_submit = Submit('submit', 'Enter Todo')
+        custom_submit.field_classes = 'btn btn-success'
+        self.helper.add_input(custom_submit)
+
--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from uuid import uuid4
 
-# Create your models here.
+user_model = get_user_model()
+
+
+class Todo(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
+    text = models.TextField()
+    complete = models.BooleanField()
+    user = models.ForeignKey(user_model, on_delete=models.CASCADE)
+    parent_task = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+
