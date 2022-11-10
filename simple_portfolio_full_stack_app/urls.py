5247de369bc2d7cb27f474d663d62ccb81f55401
letusgitcommit <davidvaughn006@gmail.com>
2022-11-09 20:27:05 -0600

    changed the admin url

--- simple_portfolio_full_stack_app/urls.py
+++ simple_portfolio_full_stack_app/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('portfolio-admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('todos/', include('todos.urls')),
     path('', include('pages.urls')),
