9b6e46fc59fc6d699bafe310292ad8f82fcd8792
letusgitcommit <davidvaughn006@gmail.com>
2022-11-13 21:37:45 -0600

    added a simple logging configuration to the setup.py file

--- simple_portfolio_full_stack_app/settings.py
+++ simple_portfolio_full_stack_app/settings.py
@@ -161,3 +161,17 @@ REST_FRAMEWORK = {
         'knox.auth.TokenAuthentication'
     ]
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
